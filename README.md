03066d3ae5feb162f2d1e47bf6dd49f8d7e316df
jessicatroilo <jessica.troilo25@gmail.com>
2024-01-07 14:34:42 +0100

    ajout des icone hard skills

--- README.md
+++ README.md
@@ -4,22 +4,3 @@ Le document va être décomposer en plusieurs parties.
 
 Le but étant de mettre en oeuvre les notions apprises en HTML, CSS et PHP (Mise à jour au cours de la formation avec les nouveaux langages).
 
-## 22/11/2023 : Initialisation du CV
-
-Création des fichiers utiles pour la construction de mon CV :
-
-- les feuilles de styles CSS :
-  - reset.css (afin de partir de 0 pour la création de la mise en forme)
-  - style.css
-- le dossier images : qyui contiendra toutes les sources en images nécessaires
-- le dossier php :
-  - /datas/data.php : contient l'ensemble des données qui vont être utliser pour remplir le CV (utilisation de foreach pour récupération dans le HTML)
-  - /templates/exp.tpl.php : va contenir l'ensemble des expériences professionnelles
-  - /templates/footer.tpl.php : va contenir le footer du site présent sur l'ensemble des pages du site (si plusieurs pages)
-  - /tempales/header.tpl.php : va contenir le début du code HTML + le header présent sur l'ensemble des pages du site (si plusieurs pages)
-  - index.php : la page qui va s'afficher automatiquement, va contenir grâce à des include et require, l'ensemble des fichiers contenus dans les dossiers datas et templates.
-
-### 07/12/2023 : En-tête - Barre de Nav
-
-Mise en place de la barre de Nav
-Préparation des différents élements pour le dark mode
--- app/views/identity.tpl.php
+++ app/views/identity.tpl.php
@@ -3,10 +3,10 @@
     <section class="card-body d-flex flex-column p-5" style="background-color : white;">
 
         <h3 class="card-title text-success"> Qui-suis je ?</h3>
-        
-        <img class = "card-img"src="" alt="">
-        <p class = "card-text fs-5 p-2">Après différentes expériences dans le domaine de la maîtrise d’Ouvrage (MOA) et l'immobilier qui ont renforcé ma polyvalence et mon adaptabilité, j'ai fait le choix de me lancer dans une reconversion dans le développement web. Attirée depuis toujours par la programmation et l'informatique, j'aime résoudre des problèmes. J'utilise un eventail de langages de programmation, de framework et de librairies, j'aime apprendre et découvrir d'autres. Je me tiens au courant des technologies émérgeante afin que mon travail et les solutions proposées ssoient toujours innovants et dans une amélioration continue.</p>
-
+        <div id = "container__identity" class=" d-flex align-items-center">
+            <img id = "img__identity" class = "card-img" src="<?= $_SERVER['BASE_URI']?>/images/image-identity.jpg" alt="Image illustration femme ordinateur">
+            <p class = "card-text fs-5 p-2">Après différentes expériences dans le domaine de la maîtrise d’Ouvrage (MOA) et l'immobilier qui ont renforcé ma polyvalence et mon adaptabilité, j'ai fait le choix de me lancer dans une reconversion dans le développement web. Attirée depuis toujours par la programmation et l'informatique, j'aime résoudre des problèmes. J'utilise un eventail de langages de programmation, de framework et de librairies, j'aime apprendre et découvrir d'autres. Je me tiens au courant des technologies émérgeante afin que mon travail et les solutions proposées ssoient toujours innovants et dans une amélioration continue.</p>
+        </div>
     </section>
 </div>
 <!-- ----------------------------------- -->
