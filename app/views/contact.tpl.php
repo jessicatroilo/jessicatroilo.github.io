ce535092f01a26b122d5459cb23fc959e6becf86
jessicatroilo <jessica.troilo25@gmail.com>
2024-01-09 10:38:39 +0100

    contact

--- app/views/contact.tpl.php
+++ app/views/contact.tpl.php
@@ -2,9 +2,14 @@
     <div class="card d-flex" style= "border: none; ">
         <section id="contact" class="card-body d-flex flex-column p-5" style="background-color : #BEC6C3;">
 
-            <h3  class="card-title text-success">Contact</h3>
+            <h3 class="card-title text-success">Contact</h3>
             
-            <p class = "card-text fs-6">jessica.troilo25@gmail.com</p>
+            <a  class="link-offset-2 link-underline link-underline-opacity-0" href="mailto:jessica.troilo25@gmail.com">
+            <p class = "text-black card-text fs-6 justify-content-center p-2">
+                <img class="p-2" src="<?= $_SERVER['BASE_URI']?>/images/e-mail.png" width="55" height="55" alt="Illustration pour adresse mail">
+                jessica.troilo25@gmail.com
+            </p>
+            </a>
 
         </section>
     </div>
